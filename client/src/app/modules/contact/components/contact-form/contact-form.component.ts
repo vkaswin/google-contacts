7b623ead42c496aa658508319cd44fefe88d2cc2
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-07-27 18:11:18 +0530

    update

--- client/src/app/modules/contact/components/contact-form/contact-form.component.ts
+++ client/src/app/modules/contact/components/contact-form/contact-form.component.ts
@@ -1,52 +1,156 @@
 import { Component, OnInit } from "@angular/core";
 import { CommonModule, Location } from "@angular/common";
-import { FormBuilder, Validators } from "@angular/forms";
+import {
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+} from "@angular/forms";
 import { InputComponent } from "@/app/shared/components/input/input.component";
 
 @Component({
   selector: "app-contact-form",
   standalone: true,
-  imports: [CommonModule, InputComponent],
+  imports: [CommonModule, InputComponent, ReactiveFormsModule],
   templateUrl: "./contact-form.component.html",
   styles: [],
 })
 export class ContactFormComponent implements OnInit {
-  formControl;
+  form: FormGroup;
   isExpanded = false;
+  isSubmitted = false;
 
   constructor(private fb: FormBuilder, private location: Location) {
-    this.formControl = fb.group({
-      firstName: fb.control({}, { validators: [Validators.required] }),
-      lastName: fb.control({}, { validators: [Validators.required] }),
-      nickName: fb.control({}, { validators: [] }),
-      company: fb.control({}, { validators: [Validators.required] }),
-      jobTitle: fb.control({}, { validators: [Validators.required] }),
-      department: fb.control({}, {}),
-      email: fb.control({}, { validators: [Validators.required] }),
-      phone: fb.control({}, { validators: [Validators.required] }),
-      country: fb.control({}, {}),
-      addressLine1: fb.control({}, {}),
-      addressLine2: fb.control({}, {}),
-      state: fb.control({}, {}),
-      city: fb.control({}, {}),
-      pincode: fb.control({}, {}),
-      birthday: fb.control({}, { validators: [Validators.required] }),
-      event: fb.control({}, {}),
-      website: fb.control({}, {}),
-      relatedPeople: fb.control({}, {}),
-      chat: fb.control({}, {}),
-      notes: fb.control({}, { validators: [Validators.required] }),
+    this.form = fb.group({
+      firstName: fb.control(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.pattern(/^[a-zA-Z]*$/),
+      ]),
+      lastName: fb.control(null, [
+        Validators.min(3),
+        Validators.pattern(/^[a-zA-Z]*$/),
+      ]),
+      nickName: fb.control(null, [
+        Validators.min(3),
+        Validators.pattern(/^[a-zA-Z]*$/),
+      ]),
+      company: fb.control(null),
+      jobTitle: fb.control(null),
+      department: fb.control(null),
+      email: fb.control(null, [Validators.email]),
+      phone: fb.control(null, [
+        Validators.pattern(
+          /^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9][02-9])\s*\)|([2-9][02-9]))\s*(?:[.-]\s*)?)?([2-9][0-9]{2})\s*(?:[.-]\s*)?([0-9]{4})      /
+        ),
+      ]),
+      country: fb.control(null),
+      addressLine1: fb.control(null),
+      addressLine2: fb.control(null),
+      state: fb.control(null),
+      city: fb.control(null),
+      pincode: fb.control(null, [Validators.pattern(/^\d{6}$/)]),
+      birthday: fb.control(null, [Validators.pattern(/^\d{4}-\d{2}-\d{2}$/)]),
+      event: fb.control(null),
+      website: fb.control(null, [
+        Validators.pattern(
+          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/
+        ),
+      ]),
+      relatedPeople: fb.control(null),
+      chat: fb.control(null),
+      notes: fb.control(null),
     });
   }
 
   ngOnInit(): void {}
 
+  get firstName() {
+    return this.form.controls["firstName"] as FormControl;
+  }
+
+  get lastName() {
+    return this.form.controls["lastName"] as FormControl;
+  }
+
+  get nickName() {
+    return this.form.controls["nickName"] as FormControl;
+  }
+
+  get company() {
+    return this.form.controls["company"] as FormControl;
+  }
+
+  get jobTitle() {
+    return this.form.controls["jobTitle"] as FormControl;
+  }
+
+  get department() {
+    return this.form.controls["department"] as FormControl;
+  }
+
+  get email() {
+    return this.form.controls["email"] as FormControl;
+  }
+
+  get phone() {
+    return this.form.controls["phone"] as FormControl;
+  }
+
+  get addressLine1() {
+    return this.form.controls["addressLine1"] as FormControl;
+  }
+
+  get addressLine2() {
+    return this.form.controls["addressLine2"] as FormControl;
+  }
+
+  get state() {
+    return this.form.controls["state"] as FormControl;
+  }
+
+  get city() {
+    return this.form.controls["city"] as FormControl;
+  }
+
+  get pincode() {
+    return this.form.controls["pincode"] as FormControl;
+  }
+
+  get birthday() {
+    return this.form.controls["birthday"] as FormControl;
+  }
+
+  get event() {
+    return this.form.controls["event"] as FormControl;
+  }
+
+  get website() {
+    return this.form.controls["website"] as FormControl;
+  }
+
+  get relatedPeople() {
+    return this.form.controls["relatedPeople"] as FormControl;
+  }
+
+  get chat() {
+    return this.form.controls["chat"] as FormControl;
+  }
+
+  get notes() {
+    return this.form.controls["notes"] as FormControl;
+  }
   handleExpand() {
     this.isExpanded = !this.isExpanded;
   }
 
   handleSaveFrom() {
-    console.log("save form");
+    if (!this.isSubmitted) this.isSubmitted = true;
+
+    if (!this.form.valid) return;
+
+    console.log(this.form.value);
     this.handleCloseForm();
   }
 
--- client/src/app/shared/components/input/input.component.ts
+++ client/src/app/shared/components/input/input.component.ts
@@ -1,14 +1,22 @@
 import { CommonModule } from "@angular/common";
 import { Component, Input } from "@angular/core";
+import { FormControl, ReactiveFormsModule } from "@angular/forms";
 
 @Component({
   selector: "app-input",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: "./input.component.html",
 })
 export class InputComponent {
-  @Input() hasError = true;
+  @Input() hasError = false;
   @Input() name = "";
   @Input() label = "";
+  @Input() control = new FormControl();
+  @Input() errorMessage: Record<string, string> = {};
+
+  get errorType() {
+    if (!this.control.errors) return;
+    return Object.keys(this.control.errors)[0];
+  }
 }
