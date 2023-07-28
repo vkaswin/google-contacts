72a191949fbf86c92ec809c07b554694b701b570
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-07-28 13:09:14 +0530

    update

---
+++ server/src/controllers/user.ts
@@ -0,0 +1,8 @@
+import User from "../models/user";
+import { asyncHandler } from "../utils/asyncHandler";
+
+const signIn = asyncHandler(async (req, res) => {});
+
+const signUp = asyncHandler(async (req, res) => {});
+
+export default { signIn, signUp };
--- server/src/routes/user.ts
+++ server/src/routes/user.ts
@@ -1,5 +1,9 @@
 import { Router } from "express";
+import UserController from "../controllers/user";
 
 const router = Router();
+
+router.post("/sign-in", UserController.signIn);
+router.post("/sign-up", UserController.signUp);
 
 export default router;
