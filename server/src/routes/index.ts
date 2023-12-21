96380b033020c01c3d4a178d6398c0d2ad7d6b0c
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-12-21 23:21:29 +0530

    update

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -9,4 +9,14 @@ router.use("/api/user", UserRoutes);
 router.use("/api/contact", ContactRoutes);
 router.use("/api/label", LabelRoutes);
 
+router.get("/api/health-check", (req, res) => {
+  res.status(200).send({
+    status: "Success",
+    data: {
+      message: "Service is running smoothly",
+      version: "1.0.0",
+    },
+  });
+});
+
 export default router;
