07a0b7b252f4f8a11049339a8506b31b2528d455
aswin-13282 <aswin-13282@aswin-13282.csez.zohocorpin.com>
2023-12-21 23:25:21 +0530

    update

--- client/src/app/core/constants/apiEndPoints.ts
+++ client/src/app/core/constants/apiEndPoints.ts
@@ -2,7 +2,7 @@ import { isDevMode } from "@angular/core";
 
 const BASE_URL = isDevMode()
   ? "http://localhost:8000"
-  : "http://localhost:8000";
+  : "https://google-contacts-ashy.vercel.app";
 
 export const USER_URL = `${BASE_URL}/api/user`;
 export const CONTACT_URL = `${BASE_URL}/api/contact`;
